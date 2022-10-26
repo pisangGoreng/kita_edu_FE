b2289a16f180d1a7ea2225d187e308c947aa0b38
endy <7ofpentacles@gmail.com>
2022-10-26 18:44:17 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="text-sm font-bold underline">Hello world!</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
