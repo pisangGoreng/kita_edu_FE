5b194c6efb7b91c36245e51eea25c260fd0567a1
endy <7ofpentacles@gmail.com>
2022-11-01 22:23:08 +0700

    test add vocations images

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,30 +5,31 @@ import "react-lazy-load-image-component/src/effects/blur.css"
 
 export default function Banner() {
   return (
-    <div className="flex bg-[#07638d] font-avenir text-[#07638d] h-screen">
+    <div className="flex bg-[#07638d] font-avenir text-[#07638d]">
       <img
         src={require("../assets/images/banner-1.png")}
         alt={"image.alt"}
         style={{
           width: "100%",
-          height: "100%",
+          height: "500px",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
         }}
       />
 
-      <div className="w-[819px] text-[30px] absolute mt-[420px] mx-44">
+      <div className="w-[700px] text-[23px] absolute mt-[200px] mx-44">
         <Typed
-          className="text-[71px] font-black leading-none font-avenirblack"
+          className="text-[65px] font-black leading-none font-avenirblack"
           strings={["Gotong Royong Bangun Masa Depan"]}
           typeSpeed={80}
         />
 
-        <p className="mt-2">
-          Bersama mencetak tenaga kerja Indonesia yang terampil dan berkualitas.
+        <p className="mt-2 leading-none">
+          Bersama mencetak tenaga kerja Indonesia yang
         </p>
+        <p className="mt-2 leading-none">terampil dan berkualitas.</p>
 
-        <button className="px-4 py-2 bg-[#ffcd56]  rounded-md focus:ring-0 shadow-lg transform active:scale-75 transition-transform outline-0">
+        <button className="px-4 mt-4 py-2 bg-[#ffcd56]  rounded-md focus:ring-0 shadow-lg transform active:scale-75 transition-transform outline-0">
           Daftar Sekarang
         </button>
       </div>
